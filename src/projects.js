9e5bff982f2c923531296d2b3cc1e225ef3214ac
CEJSH <eun2763@naver.com>
2023-09-04 13:17:59 +0000

    filter

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// 프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector('.categories')
+categories.addEventListener('click', (event) => {
+    const filter = event.target.dataset.category;
+    if (filter == null) {
+        return;
+    }
+    console.log(filter);
+});
