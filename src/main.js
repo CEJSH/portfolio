42f18c86febd64c5d96303e7f6035005dde83c56
CEJSH <eun2763@naver.com>
2023-09-04 05:01:06 +0000

    js-home-opacity

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-//  Header에 페이지 아래로 스크롤 시 다크 스타일링 적용
+// Header에 페이지 아래로 스크롤 시 다크 스타일링 적용
 const header = document.querySelector(".header");
 const headerHeight = header.getBoundingClientRect().height;
-
+//  const headerHeight = header.offsetHeight;
 document.addEventListener('scroll',  () => {
     if (window.scrollY > headerHeight) {
         header.classList.add('header--dark');
@@ -9,3 +9,12 @@ document.addEventListener('scroll',  () => {
         header.classList.remove('header--dark');
     }
 });
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+
+document.addEventListener('scroll', ()=> {
+home.style.opacity = 1 - window.scrollY / homeHeight
+});
+
